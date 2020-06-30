93676661c1b03688f86f6d7c586da5426cb5260f
Ivan <Ivan.Zakharov.aob@gmail.com>
2020-06-30 09:24:48 +0300

    clear Server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -8,7 +9,19 @@ namespace Server
 {
     class Program
     {
+        class Client
+        {
+            public Socket Socket { get; set; }
+            public int ID { get; set; }
+            public Client(Socket socket)
+            {
+                Socket = socket;
+            }
+        }
+        static Random random = new Random();
         static Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        static List<Client> clients = new List<Client>();
+
         static void Main(string[] args)
         {
             Console.Title = "Server";
@@ -22,19 +35,63 @@ namespace Server
         }
         static void AcceptCallBack(IAsyncResult ar)
         {
-            Socket client = socket.EndAccept(ar);
+            Client client = new Client(socket.EndAccept(ar));
             Thread thread = new Thread(HandleClient);
             thread.Start(client);
+            clients.Add(client);
+            Console.WriteLine("New connection");
+
+            socket.BeginAccept(AcceptCallBack, null);
         }
 
         static void HandleClient(object o)
         {
-            var client = (Socket)o;
+            var client = (Client)o;
             MemoryStream ms = new MemoryStream(new byte[256], 0, 256, true, true);
+            BinaryWriter writer = new BinaryWriter(ms);
+            BinaryReader reader = new BinaryReader(ms);
 
             while (true)
             {
-                client.Receive(ms.GetBuffer());
+                ms.Position = 0;
+                try
+                {
+                    client.Socket.Receive(ms.GetBuffer());
+                }
+                catch
+                {
+                    client.Socket.Shutdown(SocketShutdown.Both);
+                    client.Socket.Disconnect(true);
+                    clients.Remove(client);
+                    Console.WriteLine($"User with ID: {client.ID} disconnected");
+                    return;
+                }
+
+                int code = reader.ReadInt32();
+
+                switch (code)
+                {
+                    case 0:
+                        int id = random.Next(0, 1001);
+                        if(clients.Find(c=>c.ID == id) == null)
+                        {
+                            writer.Write(id);
+                            client.Socket.Send(ms.GetBuffer());
+                            client.ID = id;
+                            break;  
+                        }
+                        break;
+                        
+                    case 1:
+                        foreach(var c in clients)
+                        {
+                            if (c != client)
+                            {
+                                c.Socket.Send(ms.GetBuffer());
+                            }
+                        }
+                        break;
+                }
             }
         }
     }
